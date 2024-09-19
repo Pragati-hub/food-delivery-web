3bdf5d1c54f373d2c73d6d3fc7f95dabd2247fef
pragati <pragatiguptarg4@gmail.com>
2024-09-19 06:15:39 +0530

    commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Navbar/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+
+    const [category,setCategory]=useState("All");
   return (
     <div>
      <Header />
-     <ExploreMenu/>
+     <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
