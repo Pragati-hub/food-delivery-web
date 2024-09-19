5447f4c8da5e7452bf53994b0936e4ef6654136d
pragati <pragatiguptarg4@gmail.com>
2024-09-19 21:32:45 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,24 @@ import Navbar from './Components/Navbar/Navbar'
 import {Route,Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
-//import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 //import Cart from './pages/Cart/Cart'
 //import { Router } from 'react-router-dom'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
      return(
+      <>
     <div className='app'>
       <Navbar/>
-      
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
-       {/*<Route path='/order' element={<PlaceOrder/>}/>*/}
+       <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
-    </div>
+    </div><Footer/>
+   
+    </>
      )
 }
 
